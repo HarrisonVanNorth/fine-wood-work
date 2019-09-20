f45b23bcc871e90ee0197651ff874232b4e87ef0
HarrisonVanNorth <hgvannort@gmail.com>
2019-09-19 21:25:13 -0700

    Vue Starting point

---
+++ frontend/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <NavBar/>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <SplashPage/>
+  </div>
+</template>
+
+<script>
+import SplashPage from './components/MainSite/SplashPage'
+import NavBar from './components/MainSite/NavBar'
+
+export default {
+  name: 'app',
+  components: {
+    SplashPage
+  }
+}
+</script>
+
+<style>
+#app {
+
+}
+</style>
