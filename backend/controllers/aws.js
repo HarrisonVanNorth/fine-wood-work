3a0d022e357e5c1b7493853568bf3ac2d468ec70
HarrisonVanNorth <hgvannort@gmail.com>
2019-10-07 10:52:10 -0700

    upload to bucket

--- backend/controllers/aws.js
+++ backend/controllers/aws.js
@@ -2,28 +2,31 @@ const knex = require('../db/knex.js')
 const AWS = require('aws-sdk');
 const fs = require("fs")
 AWS.config.loadFromPath(__dirname+'/../aws_config.json');
-var s3Bucket = new AWS.S3({ params: { Bucket: "fine-wood-work-182749" } });
+AWS.config.update({region: "us-west-2"})
+var s3Bucket = new AWS.S3({ params: { Bucket: "fine-wood-work-182749", ACL: 'public-read', apiVersion: '2006-03-01' } });
 const baseAWSURL = "https://s3-us-west-2.amazonaws.com/fine-wood-work-182749/"
 
 module.exports = {
+
   upload: (req, res)=> {
+    // console.log(req)
     let uploadData = {
-      Key: req.files.file.name,
-      Body: req.files.file.data,
-      ContentType: req.files.file.mimetype,
-      ACL: 'public-read'
+      Bucket: "fine-wood-work-182749",
+      Key: req.files.data.name,
+      Body: req.files.data.data,
+      ContentType: req.files.data.mimetype  
     }
-    s3Bucket.putObject(uploadData, function (err, data) {
-      if (err) {
-        return;
-      }
-      knex('images').insert({
-        img_url: baseAWSURL + uploadData.Key,
-        category: req.body.category,
-        key: "" + uploadData.Key
-      }, '*').then((results) => {
-        res.json({ images: results[0] });
-      })
+    s3Bucket.upload(uploadData, function (err, data) {
+      if (err)  throw err
+      console.log(`File uploaded successfully at ${data.Location}`)
+    // }
+      // knex('images').insert({
+      //   img_url: baseAWSURL + uploadData.Key,
+      //   category: req.body.category,
+      //   key: "" + uploadData.Key
+      // }, '*').then((results) => {
+      //   res.json({ images: results[0] });
+      // })
     });
   },
 
